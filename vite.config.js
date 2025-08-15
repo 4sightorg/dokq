e9d564897613318956d07d9ba965e9b0b9c30778
Adriel Magalona <dagsmagalona@gmail.com>
2025-08-16 00:31:44 +0800

    Initial project setup with build tools and dependencies

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  
+  server: {
+    port: 5173,
+    open: true
+  },
+  
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@services': resolve(__dirname, 'src/services'),
+      '@utils': resolve(__dirname, 'src/utils'),
+      '@styles': resolve(__dirname, 'src/styles'),
+      '@pages': resolve(__dirname, 'src/pages'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@config': resolve(__dirname, 'src/config')
+    }
+  }
+})
