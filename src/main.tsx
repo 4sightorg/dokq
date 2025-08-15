734498ffe11b38ae9b7908a5b221d09c5036e10b
Adriel Magalona <dagsmagalona@gmail.com>
2025-08-16 00:47:43 +0800

    add main application entry points and basic strucs

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+if (import.meta && (import.meta as any).env && (import.meta as any).env.PROD) {
+  const methods = ['log', 'info', 'debug', 'trace', 'table', 'group', 'groupCollapsed', 'groupEnd', 'time', 'timeEnd'] as const
+  methods.forEach((m) => {
+    ;(console as any)[m] = () => {}
+  })
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+) 
